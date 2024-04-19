ec2b161d23004218e77d30d2e614b0bf926904ef
Magbe23 <148983353+Magbe23@users.noreply.github.com>
2024-04-18 21:56:50 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
+let miDinero;
+let precioLaptop;
+let precioConDescuento = precioLaptop - (precioLaptop * .20) //Precio del portátil al 20% de descuento
 
+if (miDinero >= precioLaptop || miDinero >= precioConDescuento){
+    //La condición era verdadera. Se ejecutará el código de este bloque.
+    console.log("¡Conseguiste una nueva laptop!");
+}
+else {
+    //La condición era falsa. Se ejecutará el código de este bloque.
+    console.log("¡No puedo pagarlo, todavía!");
+}
